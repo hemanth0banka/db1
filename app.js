58300aeb5a65bd9e30cd78365f16103190f81eb7
hemanth0banka <150692843+hemanth0banka@users.noreply.github.com>
2025-10-05 10:58:41 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,30 +1,64 @@
-const express = require("express")
-const app = express()
-const mysql = require("mysql2")
+const express = require("express");
+const mysql = require("mysql2");
+const app = express();
+const port = 2002;
 const connection = mysql.createConnection({
-    host : "localhost",
+    host : "127.0.0.1",
     user : "root",
     password : "1234",
     database : "mydb"
 })
-connection.connect((err)=>{
-    if(err)
+connection.connect((e)=>{
+    if(e) 
     {
-        console.log(err)
+        console.log(e)
         return
     }
-    console.log("connected successfully")
-    const q = `create table students(id int,name varchar(20));`
-    connection.execute(q,(e)=>{
-        if(e){
+    console.log(`Connected Successfully.`)
+    const query1 = `create table Users(id int AUTO_INCREMENT primary key,name varchar(255),email varchar(255))`;
+    connection.execute(query1,(e)=>{
+        if(e)
+        {
             console.log(e)
-            connection.end
+            connection.end()
             return
         }
-        console.log(`table created`)
+        console.log(`Users table created Successfully.`)
+        const query = `create table Buses(id int AUTO_INCREMENT primary key,busNumber int,totalSeats int,availableSeats int)`
+        connection.execute(query,(e)=>{
+            if(e)
+            {
+                console.log(e)
+                connection.end()
+                return
+            }
+            console.log(`Buses table Created Successfully.`)
+            const query = `create table Bookings(id int AUTO_INCREMENT primary key,seatNumber int)`
+            connection.execute(query,(e)=>{
+                if(e)
+                {
+                    console.log(e)
+                    connection.end()
+                    return
+                }
+                console.log(`Bookings table Created Successfully.`)
+                const query = `create table Payments(id int AUTO_INCREMENT primary key,amountPaid int,paymentStatus varchar(255))`
+                connection.execute(query,(e)=>{
+                    if(e)
+                    {
+                        console.log(e)
+                        connection.end()
+                        return
+                    }
+                    console.log(`Payments table created successfully`)
+                    connection.end(()=>{console.log(`Connection Ended...`)})
+                })
+            })
+        })
     })
 })
-app.get("/",(req,res)=>{
-    res.send(`Hello world`);
+
+app.listen(port,()=>{
+    console.log(`server is running ... `)
+    console.log(`listening at http://localhost:${port}`)
 })
-app.listen(2001,()=>{console.log(`Server is Listening...`)})
